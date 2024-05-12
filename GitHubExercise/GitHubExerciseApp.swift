1df2d6a9f7f2e18f9f6c6e215cba19e8e4ccab92
lorirothermel <lori@rothermel1.com>
2024-05-11 23:11:46 -0400

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by Lori Rothermel on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
